1d7a6db4c9bbd058568286cdfd99a8dd12783c2d
michael nauman <bodhiandmike@gmail.com>
2015-01-22 21:15:19 -0800

    initial commit, red car is moving

--- DragRace.js
+++ DragRace.js
@@ -1,8 +1,82 @@
-var myCar = document.getElementByID("car");
-myCar.style.left = "0px";
+/* mikeNauman
 
-window.addEventListener('keyup', function(event) {
-  if (event.keyCode === 39) {
-    this.$el.style.left = parseInt(this.$el.style.left, 10) + 1 + "px";
-   }
+Game Flow
+start game button changes UI, 
+initializes keyboard listeners
+
+pre-stage event listeners wait for both cars to be in 
+pre stage, trigger the tree.
+
+The tree is the Ground Launch Sequencer, prompts user, trigger 
+lights, need listner for a car jumping the gun and finish
+line.  Clock starts on green light
+
+need a timer function for the clock.  To be initiated by the start 
+the tree
+
+car crosses finish, saves score in array, changes UI, 
+
+Drag Race!
+Build an in-browser game that will let a player race their awesome dragster on a race-track, by using the keyboard to control it.
+
+What should it do?
+
+When the game starts, the car should be in the pre-staging area and the first set of yellow lights should be on (pre-stage)
+The game should start with a dragster that has its engine off at the dragstrip
+The player should be able to start the engine with one of the keyboard keys
+After starting the engine, the player can move the dragster into the staging area, crossing the pre-staging line
+Once the dragster is in the staging area, the "stage" light should turn on
+At this point, the next 3 sets of light should turn on, with 0.5 seconds between each set
+The second to last set is the set of green lights, which should indicate the start of the race
+If the player attempts to drive the car before the green lights are turned on, the last set of lights should be lit in red, and the race is over
+If the light is green, the player should be able to race their dragster down the racetrack
+Crossing the finish line is the end of the race
+Use HTML, CSS, some images from google, and JS to build the game
+Bonuses
+
+Calculate the drag race time
+Restart the race keeping previous race times in a table
+Save the drag race time and show the fastest drag race time
+Super-bonus
+
+Two-player drag race (use different parts of the keyboard to let two players control the game)
+Add an AI that races against you
+Give the AI different difficulty levels (easy, medium, hard) where each level is a different speed
+Add a cheat code (http://en.wikipedia.org/wiki/Konami_Code) which automatically lets you win the race.
+Super-duper-uber bonus
+
+Turn the drag race into a race track!
+Implement a way to draw an arbitrary race track, defined via a two-dimensional array, with coordinates in the cartesian plane
+The two-dimensional array should be represented on screen by having the race-track be drawn based on it
+The AI should be able to navigate the race track, and your car should only be able to move along the race track
+Status API Training Shop Blog About
+*/
+
+window.onload = function() {
+
+
+// wrap this in a function moveCar with (carName, key)
+
+// ----------------------------------------------
+// this moves the red block "car" left and right
+var myDiv = document.getElementById("car1");
+myDiv.style.left = "0px";
+myDiv.style.right = "0px";
+
+window.addEventListener('keyup', function(event){
+
+	if (event.keyCode === 39) {
+		myDiv.style.left = parseInt(myDiv.style.left, 10) + 25 + "px";
+	}
+
+	if (event.keyCode === 37) {
+		myDiv.style.left = parseInt(myDiv.style.left, 10) - 25 + "px";
+	}
+
 });
+
+
+
+
+};
+
